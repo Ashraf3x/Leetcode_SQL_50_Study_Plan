ffd3957e12546c94217025d842dc498bdf4ffac3
Ashraf3x <ahmedashrafmohamed112@gmail.com>
2026-06-09 08:00:57 +0300

    Solve problem 10: Average-Time-of-Process-per-Machine

--- Basic-Joins/10-Average-Time-of-Process-per-Machine/solution.sql
+++ Basic-Joins/10-Average-Time-of-Process-per-Machine/solution.sql
@@ -1,6 +1,5 @@
 select a.machine_id, round(avg(b.timestamp - a.timestamp), 3) as processing_time
-from activity a
-join activity b 
+from activity a join activity b 
  on a.machine_id = b.machine_id 
  and a.process_id = b.process_id 
  and a.activity_type = 'start' 
