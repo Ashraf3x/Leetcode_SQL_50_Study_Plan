feee575f5053dda92e17790ee07bc6b3a5fd70e2
Ashraf3x <ahmedashrafmohamed112@gmail.com>
2026-06-10 03:50:21 +0300

    Solve problem 11: Employee Bonus

--- Basic-Joins/11-Employee-Bonus/solution.sql
+++ Basic-Joins/11-Employee-Bonus/solution.sql
@@ -1,3 +1,4 @@
--- SQL Solution
--- Write your MySQL query statement below
-
+select name , bonus       
+from Employee left join Bonus
+on Employee.empId = Bonus.empId
+where Bonus < 1000 or Bonus is null;
