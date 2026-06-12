5399ef6f95b031a9620b494ae3a234cecd206d25
Ashraf3x <ahmedashrafmohamed112@gmail.com>
2026-06-12 16:40:58 +0300

    Solve problem 13: Managers with at Least 5 Direct Reports

--- Basic-Joins/13-Managers-with-at-Least-5-Direct-Reports/solution.sql
+++ Basic-Joins/13-Managers-with-at-Least-5-Direct-Reports/solution.sql
@@ -1,3 +1,8 @@
--- SQL Solution
--- Write your MySQL query statement below
-
+select name
+from employee
+where id in (
+    select managerid
+    from employee
+    group by managerid
+    having count(id) >= 5
+);
