43c7be85728330119c2f906ac803445811e8a718
Ashraf3x <ahmedashrafmohamed112@gmail.com>
2026-06-13 23:09:58 +0300

    Solve problem 14: Confirmation-Rate

--- Basic-Joins/14-Confirmation-Rate/solution.sql
+++ Basic-Joins/14-Confirmation-Rate/solution.sql
@@ -1,3 +1,6 @@
--- SQL Solution
--- Write your MySQL query statement below
-
+select 
+    s.user_id,
+    round(ifnull(avg(if(c.action = 'confirmed', 1, 0)), 0), 2) as confirmation_rate
+from signups s
+left join confirmations c on s.user_id = c.user_id
+group by s.user_id;
