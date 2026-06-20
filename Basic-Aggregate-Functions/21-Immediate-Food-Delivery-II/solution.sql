49bed6f2bbb20f17466fcf241b30468d36dfce2e
Ashraf3x <ahmedashrafmohamed112@gmail.com>
2026-06-20 10:28:06 +0300

    Solve problem 21: Immediate Food Delivery II

--- Basic-Aggregate-Functions/21-Immediate-Food-Delivery-II/solution.sql
+++ Basic-Aggregate-Functions/21-Immediate-Food-Delivery-II/solution.sql
@@ -1,3 +1,8 @@
--- SQL Solution
--- Write your MySQL query statement below
-
+select 
+    round(avg(case when order_date = customer_pref_delivery_date then 100.0 else 0.0 end), 2) as immediate_percentage
+from delivery d1
+where order_date = (
+    select min(order_date)
+    from delivery d2
+    where d1.customer_id = d2.customer_id
+);
