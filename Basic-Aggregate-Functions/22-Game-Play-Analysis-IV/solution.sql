c2f72412dd7480cd456175984f489ce75c6dfe94
Ashraf3x <ahmedashrafmohamed112@gmail.com>
2026-06-21 11:25:41 +0300

    Solve problem 22: Game-Play-Analysis-IV

--- Basic-Aggregate-Functions/22-Game-Play-Analysis-IV/solution.sql
+++ Basic-Aggregate-Functions/22-Game-Play-Analysis-IV/solution.sql
@@ -1,3 +1,9 @@
--- SQL Solution
--- Write your MySQL query statement below
-
+select round(count(a2.player_id) * 1.0 / count(distinct a1.player_id), 2) as fraction
+from (
+    select player_id, min(event_date) as first_date
+    from activity
+    group by player_id
+) a1
+left join activity a2
+    on a1.player_id = a2.player_id 
+    and datediff(day, a1.first_date, a2.event_date) = 1;
