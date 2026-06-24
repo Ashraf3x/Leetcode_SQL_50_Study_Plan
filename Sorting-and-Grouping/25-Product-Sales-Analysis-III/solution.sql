0f01ac036684813b5cd07162f78405b993163390
Ashraf3x <ahmedashrafmohamed112@gmail.com>
2026-06-24 20:54:14 +0300

    Solve problem 25: Sorting-and-Grouping\25-Product-Sales-Analysis-III

--- Sorting-and-Grouping/25-Product-Sales-Analysis-III/solution.sql
+++ Sorting-and-Grouping/25-Product-Sales-Analysis-III/solution.sql
@@ -1,3 +1,3 @@
--- SQL Solution
--- Write your MySQL query statement below
-
+select product_id, year as first_year, quantity, price 
+from sales s
+where year = (select  min(year) from  sales where product_id = s.product_id);
