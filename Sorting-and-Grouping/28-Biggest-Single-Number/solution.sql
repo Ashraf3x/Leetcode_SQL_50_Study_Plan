a3c6133fa05573b9276bafdc0bf7bed84ddce5ae
Ashraf3x <ahmedashrafmohamed112@gmail.com>
2026-06-26 16:07:24 +0300

    Solve problem 28: Biggest-Single-Number

--- Sorting-and-Grouping/28-Biggest-Single-Number/solution.sql
+++ Sorting-and-Grouping/28-Biggest-Single-Number/solution.sql
@@ -1,3 +1,7 @@
--- SQL Solution
--- Write your MySQL query statement below
-
+select max(num) as num
+from (
+    select num
+    from mynumbers
+    group by num
+    having count(num) = 1
+) as t;
