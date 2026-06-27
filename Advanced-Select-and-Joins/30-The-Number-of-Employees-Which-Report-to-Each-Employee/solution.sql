b93cd334663045ad56ad3d3eb974d278378ca4c4
Ashraf3x <ahmedashrafmohamed112@gmail.com>
2026-06-28 02:07:42 +0300

    Solve problem 30: The-Number-of-Employees-Which-Report-to-Each-Employee

--- Advanced-Select-and-Joins/30-The-Number-of-Employees-Which-Report-to-Each-Employee/solution.sql
+++ Advanced-Select-and-Joins/30-The-Number-of-Employees-Which-Report-to-Each-Employee/solution.sql
@@ -1,3 +1,13 @@
--- SQL Solution
--- Write your MySQL query statement below
-
+select 
+    m.employee_id, 
+    m.name, 
+    count(e.employee_id) as reports_count, 
+    cast(round(avg(e.age * 1.0), 0) as int) as average_age
+from employees m
+join employees e 
+    on m.employee_id = e.reports_to
+group by 
+    m.employee_id, 
+    m.name
+order by 
+    m.employee_id;
