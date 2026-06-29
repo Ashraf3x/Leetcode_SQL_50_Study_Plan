030ff8ea87722bdc3d49782e469296f1a652b6b5
Ashraf3x <ahmedashrafmohamed112@gmail.com>
2026-06-29 13:54:07 +0300

    Solve problem 31: Primary-Department-for-Each-Employee

--- Advanced-Select-and-Joins/31-Primary-Department-for-Each-Employee/solution.sql
+++ Advanced-Select-and-Joins/31-Primary-Department-for-Each-Employee/solution.sql
@@ -1,3 +1,9 @@
--- SQL Solution
--- Write your MySQL query statement below
-
+select employee_id, department_id
+from employee
+where primary_flag = 'y' 
+or employee_id in (
+    select employee_id 
+    from employee 
+    group by employee_id 
+    having count(employee_id) = 1
+)
