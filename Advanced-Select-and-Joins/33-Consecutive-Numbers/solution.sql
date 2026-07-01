4729cae63c3ebdbc3f3e71dd740b0be1d497ac8c
Ashraf3x <ahmedashrafmohamed112@gmail.com>
2026-07-01 17:32:15 +0300

    Solve problem 33: Consecutive-Numbers

--- Advanced-Select-and-Joins/33-Consecutive-Numbers/solution.sql
+++ Advanced-Select-and-Joins/33-Consecutive-Numbers/solution.sql
@@ -1,3 +1,8 @@
--- SQL Solution
--- Write your MySQL query statement below
-
+select distinct num as consecutivenums
+from (
+    select num,
+           lag(num, 1) over(order by id) as num1,
+           lag(num, 2) over(order by id) as num2
+    from logs
+) as t
+where num = num1 and num = num2
