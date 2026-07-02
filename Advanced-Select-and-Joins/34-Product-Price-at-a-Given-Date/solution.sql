750c142b6c16238f890f15f2459fed8c318b77c8
Ashraf3x <ahmedashrafmohamed112@gmail.com>
2026-07-02 05:40:54 +0300

    Solve problem 34: Product-Price-at-a-Given-Date

--- Advanced-Select-and-Joins/34-Product-Price-at-a-Given-Date/solution.sql
+++ Advanced-Select-and-Joins/34-Product-Price-at-a-Given-Date/solution.sql
@@ -1,3 +1,16 @@
--- SQL Solution
--- Write your MySQL query statement below
-
+select product_id, new_price as price
+from (
+    select product_id, new_price, 
+           row_number() over(partition by product_id order by change_date desc) as rn
+    from products
+    where change_date <= '2019-08-16'
+) as t
+where rn = 1
+union
+select distinct product_id, 10 as price
+from products
+where product_id not in (
+    select product_id
+    from products
+    where change_date <= '2019-08-16'
+)
