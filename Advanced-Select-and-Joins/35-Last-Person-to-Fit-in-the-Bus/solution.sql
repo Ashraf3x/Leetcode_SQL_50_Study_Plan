c1eaa395bf68662fec8ffac1ba8b7550b8e9cf8b
Ashraf3x <ahmedashrafmohamed112@gmail.com>
2026-07-04 04:18:01 +0300

    Solve problem 35: Last-Person-to-Fit-in-the-Bus

--- Advanced-Select-and-Joins/35-Last-Person-to-Fit-in-the-Bus/solution.sql
+++ Advanced-Select-and-Joins/35-Last-Person-to-Fit-in-the-Bus/solution.sql
@@ -1,3 +1,8 @@
--- SQL Solution
--- Write your MySQL query statement below
-
+select top 1 person_name
+from (
+    select person_name, 
+           sum(weight) over(order by turn) as total_weight
+    from queue
+) as t
+where total_weight <= 1000
+order by total_weight desc
