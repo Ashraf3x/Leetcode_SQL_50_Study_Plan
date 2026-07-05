f0db75a1a43bc623c83b40fb4734e06c59d28372
Ashraf3x <ahmedashrafmohamed112@gmail.com>
2026-07-05 05:23:59 +0300

    Solve problem 37: Employees Whose Manager Left the Company

--- Subqueries/37-Employees-Whose-Manager-Left-the-Company/solution.sql
+++ Subqueries/37-Employees-Whose-Manager-Left-the-Company/solution.sql
@@ -1,3 +1,5 @@
--- SQL Solution
--- Write your MySQL query statement below
-
+select employee_id
+from employees
+where salary < 30000
+and manager_id not in (select employee_id from employees)
+order by employee_id
