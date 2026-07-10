c3c6353751b6124a0b38f356ce8f0424614c64c2
Ashraf3x <ahmedashrafmohamed112@gmail.com>
2026-07-11 01:49:25 +0300

    Solve problem 43: Department-Top-Three-Salaries

--- Subqueries/43-Department-Top-Three-Salaries/solution.sql
+++ Subqueries/43-Department-Top-Three-Salaries/solution.sql
@@ -1,3 +1,12 @@
--- SQL Solution
--- Write your MySQL query statement below
-
+with cte as (
+    select 
+        d.name as department,
+        e.name as employee,
+        e.salary,
+        dense_rank() over(partition by e.departmentid order by e.salary desc) as rnk
+    from employee e
+    join department d on e.departmentid = d.id
+)
+select department, employee, salary
+from cte
+where rnk <= 3
