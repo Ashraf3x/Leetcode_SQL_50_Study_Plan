6ea6a72e7d7329e8c48de8993941b13700e6c79d
Ashraf3x <ahmedashrafmohamed112@gmail.com>
2026-07-13 03:23:58 +0300

    Solve problem 45: Patients With a Condition

--- Advanced-String-Functions-Regex-Clause/45-Patients-With-a-Condition/solution.sql
+++ Advanced-String-Functions-Regex-Clause/45-Patients-With-a-Condition/solution.sql
@@ -1,3 +1,3 @@
--- SQL Solution
--- Write your MySQL query statement below
-
+select patient_id, patient_name, conditions
+from patients
+where conditions like 'diab1%' or conditions like '% diab1%'
