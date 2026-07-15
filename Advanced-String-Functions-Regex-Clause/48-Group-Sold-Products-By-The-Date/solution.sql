a9378c35c64b17987e676d47140a91392195c001
Ashraf3x <ahmedashrafmohamed112@gmail.com>
2026-07-16 00:27:30 +0300

    Solve problem 48: Group-Sold-Products-By-The-Date

--- Advanced-String-Functions-Regex-Clause/48-Group-Sold-Products-By-The-Date/solution.sql
+++ Advanced-String-Functions-Regex-Clause/48-Group-Sold-Products-By-The-Date/solution.sql
@@ -1,3 +1,7 @@
--- SQL Solution
--- Write your MySQL query statement below
-
+select 
+    sell_date, 
+    count(distinct product) as num_sold, 
+    string_agg(product, ',') within group (order by product) as products
+from (select distinct sell_date, product from activities) as t
+group by sell_date
+order by sell_date;
