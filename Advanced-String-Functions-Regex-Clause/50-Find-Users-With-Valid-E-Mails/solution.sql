aa00c030aa3d42f0478641ede7e5a5a68d0e016c
Ashraf3x <ahmedashrafmohamed112@gmail.com>
2026-07-17 11:23:22 +0300

    Solve problem 49: Find-Users-With-Valid-E-Mails

--- Advanced-String-Functions-Regex-Clause/50-Find-Users-With-Valid-E-Mails/solution.sql
+++ Advanced-String-Functions-Regex-Clause/50-Find-Users-With-Valid-E-Mails/solution.sql
@@ -1,3 +1,4 @@
--- SQL Solution
--- Write your MySQL query statement below
-
+select user_id, name, mail
+from Users
+where mail like '[a-z]%@leetcode.com' 
+and mail not like '%[^a-z0-9_.-]%@leetcode.com'
